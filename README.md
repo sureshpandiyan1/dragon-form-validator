a93caf4351af120ce6d0fae4cfcfcfd33aafc89f
sureshpandiyan1 <suresh.pandiyan1@outlook.com>
2022-09-16 00:06:00 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Description
+
+The dragon form validator module is the best way to validate your form elements and is suitable for every web app framework like a flask, Django, and other uses. It will check the form values, whether it's fit your form or not.
+
+## Highlights about Dragon Form Validator:
+
+tested 200+ fake emails - It filters out easily
+gives you excellent validation results without fail
+game changer for your forms
+computer understand format in boolean - 0 or 1
+
+## copyright & License
+
+dragon form validator | Copyright @ Suresh P | MIT
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='dragon_form_validator',
+    version='1.0.0',
+    license='MIT',
+    py_modules=['dragon_form_validator'],
+    description='The dragon form validator module is the best way to validate your form elements and is suitable for '
+                'every web app framework like a flask, Django, and other uses. It will check the form values, '
+                'whether its fit your form or not.',
+    author='Suresh P',
+    author_email='suresh.pandiyan1@outlook.com',
+    url='https://github.com/sureshpandiyan1/dragon-form-validator'
+)
